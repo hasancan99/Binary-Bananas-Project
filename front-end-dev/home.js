85ae3611663c34c4fb812b3dd03dddaab183e2a4
Hasancan <hasancan_cifci@hotmail.co.uk>
2023-06-13 22:40:54 +0100

    added a submit form to input a username

--- front-end-dev/home.js
+++ front-end-dev/home.js
@@ -1,5 +1,12 @@
 
+/**This file is in charge of the leaderboard and username submit features.
+ * These features could all be placed inside of of the index.js in the 
+ * future to make it easily accessible as they all belong on the same page.
+ * Another option could be to bundle the two JS files.
+ */
 
+
+//retrieve leaderboard when button pressed
 document.querySelector('#revealLeaderboard').addEventListener("click", e => {
     const users = userData()
     const leaderboard = document.querySelector('#Leaderboard')
@@ -7,17 +14,23 @@ document.querySelector('#revealLeaderboard').addEventListener("click", e => {
     
     
 })
+//add user when submitted 
+document.querySelector('#userInput').addEventListener("submit", e =>{
+    event.preventDefault()
+    let userInputName= document.querySelector('#username').value
+    addUser(userInputName)
+})
 
-//
+//function to retrieve and order data for leaderboard
 const userData= async ()=>{
     try{
         const resp = await fetch('http://localhost:3000/leaderboard')
     if (resp.ok) {
         const data = await resp.json();
-        console.log(data);
+        //for loop to append each data element to leaderboard list on html
         for(let i =0; i<data.length; i++){   
-        
-            const leaderboardList =document.querySelector('#leaderboardList')
+            
+            const leaderboardList =document.querySelector('#leaderboardList') 
             const listElement = document.createElement("li")
             listElement.textContent = `Username : ${data[i].username}   Total Score : ${data[i].totalScore}`
             leaderboardList.appendChild(listElement)
@@ -31,7 +44,7 @@ const userData= async ()=>{
       }
       
 }
-
+//checks if submitted username exists, and adds to database if not
 const addUser = async(username) => {
     try {
         const resp = await fetch(`http://localhost:3000/add-user/${username}`, {
@@ -47,7 +60,7 @@ const addUser = async(username) => {
       }
     }
 
-
+//checks if username exists, and changes score of user 
 const addScore = async(username, totalScore) => {
         try {
             const resp = await fetch(`http://localhost:3000/add-totalScore/${username}/${totalScore}`, {
@@ -67,5 +80,5 @@ const addScore = async(username, totalScore) => {
            console.log(err);
           }
         }
-    
-addScore('tom', '3333333');
+/** This function can be updated in the future to add on to the users score rather than replacing it */
+

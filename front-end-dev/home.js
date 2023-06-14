1c9855e52b95d6750d386f70df12a3c7a6bc0009
elliotclowes <elliot@clowes.me>
2023-06-14 12:12:15 +0100

    user can login, register and leaderboard shown

--- front-end-dev/home.js
+++ front-end-dev/home.js
@@ -1,31 +1,46 @@
+
 /**This file is in charge of the leaderboard and username submit features.
- * These features could all be placed inside of of the index.js in the
+ * These features could all be placed inside of of the index.js in the 
  * future to make it easily accessible as they all belong on the same page.
  * Another option could be to bundle the two JS files.
  */
 
-//const { green } = require("ansi-colors");
+
 
 // Update the username form submission event.
-document.querySelector('#userInput').addEventListener("submit", async event =>{
+document.querySelector('#userInput').addEventListener("submit", async event => {
     event.preventDefault();
     let userInputName = document.querySelector('#username').value;
     
+    // Determine whether the event is for registration or login based on the clicked button's id
+    let action = event.submitter.id;
+
     // Fetch all usernames
     const resp = await fetch('http://localhost:3000/usernames');
     if (resp.ok) {
         const usernames = await resp.json();
 
-        // Check if the username already exists
-        if (usernames.includes(userInputName)) {
-            // Store the username in localStorage
-            localStorage.setItem('username', userInputName);
+        // Registering a new user
+        if (action === 'register') {
+            if (usernames.includes(userInputName)) {
+                // If the username already exists, show an error
+                alert("Username already exists.");
+            } else {
+                // If the username doesn't exist, create new user
+                addUser(userInputName);
+            }
+        } else if (action === 'login') {
+            // Log in existing user
+            if (usernames.includes(userInputName)) {
+                // Store the username in localStorage
+                localStorage.setItem('username', userInputName);
 
-            // Redirect to index.html
-            window.location.href = 'index.html';
-        } else {
-            // If the username doesn't exist, show an error
-            alert("Username does not exist.");
+                // Redirect to index.html
+                window.location.href = 'index.html';
+            } else {
+                // If the username doesn't exist, show an error
+                alert("Username does not exist.");
+            }
         }
     } else {
         console.log("Error: " + resp.status);
@@ -35,108 +50,79 @@ document.querySelector('#userInput').addEventListener("submit", async event =>{
 
 
 
-document.querySelector('#userInput').addEventListener("submit", async event =>{
-    event.preventDefault();
-    let userInputName = document.querySelector('#username').value;
-    
-    // Check if the username already exists
-    try {
-        const resp = await fetch(`http://localhost:3000/check-user/${userInputName}`);
-        if (resp.ok) {
-            const data = await resp.json();
-            if (data.userExists) {
-                localStorage.setItem('username', userInputName); // Store the username
-
-                // Redirect to index.html
-                window.location.href = 'index.html';
-            } else {
-                alert("User does not exist.");
-            }
-        } else {
-            throw "Error: http status code = " + resp.status;
-        }
-    } catch (err) {
-        console.log(err);
-    }
-})
-
-
-
 //retrieve leaderboard when button pressed
 document.querySelector("#revealLeaderboard").addEventListener("click", (e) => {
-  const users = userData("emmajones");
-  const leaderboard = document.querySelector("#Leaderboard");
-  leaderboard.hidden = false;
-});
-//add user when submitted
-document.querySelector("#userInput").addEventListener("submit", (e) => {
-  event.preventDefault();
-  let userInputName = document.querySelector("#username").value;
-  addUser(userInputName);
-});
-
-
+    const users = userData("emmajones");
+    const leaderboard = document.querySelector("#Leaderboard");
+    leaderboard.hidden = false;
+  });
+  //add user when submitted
+  document.querySelector("#userInput").addEventListener("submit", (e) => {
+    event.preventDefault();
+    let userInputName = document.querySelector("#username").value;
+    addUser(userInputName);
+  });
 
 
 //function to retrieve and order data for leaderboard
 const userData = async (username) => {
-  try {
-    const resp = await fetch("http://localhost:3000/leaderboard/");
-    if (resp.ok) {
-      const data = await resp.json();
-      //set a counter to work out peoples position
-      let positionCount = 1;
-      //for loop to append each data element to leaderboard list on html
-      for (let i = 0; i < data.length; i++) {
-        if (i != 0 && data[i].totalScore != data[i - 1].totalScore) {
-          positionCount++;
-        }
-
-        const leaderboardList = document.querySelector("#leaderboardList");
-        const listElement = document.createElement("li");
-        listElement.textContent = `${ordinal_suffix_of(
-          positionCount
-        )} Username : ${data[i].username}   Total Score : ${
-          data[i].totalScore
-        }`;
-        if (username === data[i]["username"]) {
-          listElement.style.backgroundColor = "green";
-          const position = i;
-          const h3 = document.createElement("h3");
-          h3.textContent = `Well done in you are in ${ordinal_suffix_of(
+    try {
+      const resp = await fetch("http://localhost:3000/leaderboard/");
+      if (resp.ok) {
+        const data = await resp.json();
+        //set a counter to work out peoples position
+        let positionCount = 1;
+        //for loop to append each data element to leaderboard list on html
+        for (let i = 0; i < data.length; i++) {
+          if (i != 0 && data[i].totalScore != data[i - 1].totalScore) {
+            positionCount++;
+          }
+  
+          const leaderboardList = document.querySelector("#leaderboardList");
+          const listElement = document.createElement("li");
+          listElement.textContent = `${ordinal_suffix_of(
             positionCount
-          )} position!`;
-          leaderboardList.prepend(h3);
+          )} Username : ${data[i].username}   Total Score : ${
+            data[i].totalScore
+          }`;
+          if (username === data[i]["username"]) {
+            listElement.style.backgroundColor = "green";
+            const position = i;
+            const h3 = document.createElement("h3");
+            h3.textContent = `Well done in you are in ${ordinal_suffix_of(
+              positionCount
+            )} position!`;
+            leaderboardList.prepend(h3);
+          }
+          leaderboardList.appendChild(listElement);
         }
-        leaderboardList.appendChild(listElement);
+        return data;
+      } else {
+        throw "Error: http status code = " + resp.status;
       }
-      return data;
-    } else {
-      throw "Error: http status code = " + resp.status;
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    console.log(err);
+  };
+  
+  // function to add suffix to users position in leaderboard
+  function ordinal_suffix_of(i) {
+    var j = i % 10,
+      k = i % 100;
+    if (j == 1 && k != 11) {
+      return i + "st";
+    }
+    if (j == 2 && k != 12) {
+      return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+      return i + "rd";
+    }
+    return i + "th";
   }
-};
 
-// function to add suffix to users position in leaderboard
-function ordinal_suffix_of(i) {
-  var j = i % 10,
-    k = i % 100;
-  if (j == 1 && k != 11) {
-    return i + "st";
-  }
-  if (j == 2 && k != 12) {
-    return i + "nd";
-  }
-  if (j == 3 && k != 13) {
-    return i + "rd";
-  }
-  return i + "th";
-}
-
+  
 //checks if submitted username exists, and adds to database if not
-
 const addUser = async(username) => {
     try {
         const resp = await fetch(`http://localhost:3000/add-user/${username}`, {
@@ -154,30 +140,27 @@ const addUser = async(username) => {
       } catch (err) {
        console.log(err);
       }
-  }
-};
-
-//checks if username exists, and changes score of user
-const addScore = async (username, totalScore) => {
-  try {
-    const resp = await fetch(
-      `http://localhost:3000/add-totalScore/${username}/${totalScore}`,
-      {
-        method: "POST",
-        body: JSON.stringify({}),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    if (resp.ok) {
-      console.log(`${totalScore} successfully added`);
-    } else {
-      throw "Error: http status code = " + resp.status;
     }
-  } catch (err) {
-    console.log(err);
-  }
-};
+
+//checks if username exists, and changes score of user 
+const addScore = async(username, totalScore) => {
+        try {
+            const resp = await fetch(`http://localhost:3000/add-totalScore/${username}/${totalScore}`, {
+                method: "POST",
+                body: JSON.stringify({}),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  }
+            })
+            if (resp.ok) {
+                console.log(`${totalScore} successfully added`);
+            } else {
+            throw "Error: http status code = " + resp.status;
+           }
+          } catch (err) {
+           console.log(err);
+          }
+        }
 /** This function can be updated in the future to add on to the users score rather than replacing it */
+

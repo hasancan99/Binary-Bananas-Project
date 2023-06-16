0725222477de25679332ed7282411cee63a349a1
elliotclowes <elliot@clowes.me>
2023-06-16 13:12:25 +0100

    changed API back to remote instead of local

--- front-end-dev/home.js
+++ front-end-dev/home.js
@@ -13,7 +13,7 @@ document.querySelector('#userInput').addEventListener("submit", async event => {
   let action = event.submitter.id;
 
   // Fetch all usernames
-  const resp = await fetch('http://localhost:3000/usernames');
+  const resp = await fetch('https://thedinochase.onrender.com/usernames');
   if (resp.ok) {
       const usernames = await resp.json();
 
@@ -54,7 +54,7 @@ document.querySelector('#userInput').addEventListener("submit", async event => {
 // Function to retrieve and order data for leaderboard
 const userData = async (username) => {
   try {
-    const resp = await fetch("http://localhost:3000/leaderboard/");
+    const resp = await fetch("https://thedinochase.onrender.com/leaderboard/");
     if (resp.ok) {
       const data = await resp.json();
       // Set a counter to work out people's position
@@ -127,7 +127,7 @@ const userData = async (username) => {
 //checks if submitted username exists, and adds to database if not
 const addUser = async(username) => {
     try {
-        const resp = await fetch(`http://localhost:3000/add-user/${username}`, {
+        const resp = await fetch(`https://thedinochase.onrender.com/add-user/${username}`, {
             method: "POST"
         })
         if (resp.ok) {
@@ -147,7 +147,7 @@ const addUser = async(username) => {
 //checks if username exists, and changes score of user 
 const addScore = async(username, totalScore) => {
         try {
-            const resp = await fetch(`http://localhost:3000/add-totalScore/${username}/${totalScore}`, {
+            const resp = await fetch(`https://thedinochase.onrender.com/add-totalScore/${username}/${totalScore}`, {
                 method: "POST",
                 body: JSON.stringify({}),
                 headers: {

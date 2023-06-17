a252f9f24311da85a3c0ded4c8f34932788064ba
elliotclowes <elliot@clowes.me>
2023-06-17 10:24:30 +0100

    Ready for Heroku deployment

--- back-end-dev/index.js
+++ back-end-dev/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./app")
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
